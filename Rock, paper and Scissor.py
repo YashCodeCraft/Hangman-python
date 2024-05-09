b5b2e5430c482b9ce88c547accf0fb0f00bf6ffe
YashCodeCraft <152872456+YashCodeCraft@users.noreply.github.com>
2024-05-09 09:29:55 +0530

    Rock, Paper and Scissor.py
    This is a interactive Python program for playing Rock, Paper, Scissors. The program allows users to play against the computer, which generates its choice randomly. It includes error handling for invalid inputs and displays the result of each round. Additionally, it provides a tally of wins, losses, and ties throughout the game. You can easily download and run the program to enjoy the classic game of Rock, Paper, Scissors!

--- Rock, paper and Scissor.py
+++ Rock, paper and Scissor.py
@@ -1,64 +1,73 @@
 import random
 
-MAX_ROUNDS = 11
-MIN_ROUNDS = 3
+MAX_ROUNDS = 13
+MIN_ROUNDS = 5
 possibilities = ['r', 'p', 's']
 
 
 def round_played():
     while True:
-        rounds = input(f"Total number of rounds you like to Play ({MIN_ROUNDS}-{MAX_ROUNDS}): ")
+        rounds = input(f"How many rounds would you like to Play? ({MIN_ROUNDS}-{MAX_ROUNDS}): ")
         if rounds.isdigit():
             rounds = int(rounds)
             if MIN_ROUNDS <= rounds <= MAX_ROUNDS:
                 break
             else:
-                print(f"you should enter between {MIN_ROUNDS} to {MAX_ROUNDS}!")
+                print(f"Please enter value between {MIN_ROUNDS} and {MAX_ROUNDS}!")
         else:
-            print("Please enter in number!!")
+            print("Please enter the valid number!!")
     return rounds
 
 
 def play():
-    input("Welcome to rock, paper and scissor! Click enter to continue..")
+    print()
+    print("Welcome to rock, paper and scissor! \n1.You can choose the number of rounds you wish to play.")
+    print("2.The one who wins the most rounds, will be a winner!")
+    print()
+    input("Click enter to continue..")
+    print()
     comp_count, user_count, tie_count = 0, 0, 0
     tot_round = round_played()
     for Round in range(tot_round):
         print()
-        print(f"Round {Round + 1}:")
-        comp_guess = random.choice(["r", "p", "s"])
-        user_guess = input("Guess 'r' as a rock or 'p' as a paper or 's' as a scissor---> ").lower()
-        if user_guess not in possibilities:
-            print("Invalid!!!. Guess a valid value in Next round...")
-        else:
-            if user_guess == comp_guess:
-                print(f"Comp = {comp_guess}\n<<<---TIE--->>>")
-                tie_count += 1
-                print(f"No of ties: {tie_count}")
-                print(f"No of wins by you: {user_count}")
-                print(f"No of wins by comp: {comp_count}")
-            elif is_win(user_guess, comp_guess):
-                print(f"Comp = {comp_guess}\n<<<---!!|YOU WIN|!!--->>>")
-                user_count += 1
-                print(f"No of ties: {tie_count}")
-                print(f"No of wins by you: {user_count}")
-                print(f"No of wins by comp: {comp_count}")
-            elif is_loss(user_guess, comp_guess):
-                print(f"Comp = {comp_guess}\n<<<---xxX-COMP WON-Xxx--->>>")
-                comp_count += 1
-                print(f"No of ties: {tie_count}")
-                print(f"No of wins by you: {user_count}")
-                print(f"No of wins by comp: {comp_count}")
+        print(f"ROUND {Round + 1}:")
+        while True:
+            comp_guess = random.choice(["r", "p", "s"])
+            user_guess = input("Guess 'r' as a ROCK or 'p' as a PAPER or 's' as a SCISSOR---> ").lower()
+            if user_guess not in possibilities:
+                print("Invalid!!!. Guess a valid value..!")
+            else:
+                if user_guess == comp_guess:
+                    print(f"Comp = {comp_guess}\n<<<---TIE--->>>")
+                    tie_count += 1
+                    print(f"Number of ties: {tie_count}")
+                    print(f"Number of wins by you: {user_count}")
+                    print(f"Number of wins by comp: {comp_count}")
+                    break
+                elif is_win(user_guess, comp_guess):
+                    print(f"Comp = {comp_guess}\n<<<---!!|YOU WIN|!!--->>>")
+                    user_count += 1
+                    print(f"Number of ties: {tie_count}")
+                    print(f"Number of wins by you: {user_count}")
+                    print(f"Number of wins by comp: {comp_count}")
+                    break
+                elif is_loss(user_guess, comp_guess):
+                    print(f"Comp = {comp_guess}\n<<<---xxX-COMP WIN-Xxx--->>>")
+                    comp_count += 1
+                    print(f"Number of ties: {tie_count}")
+                    print(f"Number of wins by you: {user_count}")
+                    print(f"Number of wins by comp: {comp_count}")
+                    break
     print()
     print(f"Computer = {comp_count}")
     print(f"You = {user_count}")
     print(f"Ties = {tie_count}")
     if comp_count > user_count:
         print(f"Total round = {tot_round}")
-        print("Better luck next time..Computer has won the match.....!!!")
+        print("Better luck next time..Computer has won the Game.....!!!")
     elif comp_count < user_count:
         print(f"Total round = {tot_round}")
-        print("BANG!!!..You won the match...!")
+        print("BANG!!!..You won the Game...!")
     elif comp_count == user_count:
         print(f"Total round = {tot_round}")
         print("The match was tie..!")
